aa4dad8c948516e6d334f61e9be396bbb6e17ef5
harshit-7132 <harshitvyas880@gmail.com>
2025-05-15 13:27:04 +0530

    Deploy to Railway

--- lib/db.dart
+++ lib/db.dart
@@ -2,18 +2,26 @@ import 'dart:io';
 import 'package:postgres/postgres.dart';
 
 class Database {
-  static final _connection = PostgreSQLConnection(
-    Platform.environment['DB_HOST'] ?? 'localhost',
-    int.parse(Platform.environment['DB_PORT'] ?? '5432'),
-    Platform.environment['DB_NAME'] ?? 'user_management',  
-    username: Platform.environment['DB_USER'] ?? 'luffy7132',
-    password: Platform.environment['DB_PASSWORD'] ?? 'password7132',
-  );
+  static PostgreSQLConnection? _connection;
 
   static Future<PostgreSQLConnection> getConnection() async {
-    if (_connection.isClosed) {
-      await _connection.open();
+    if (_connection == null || _connection!.isClosed) {
+      _connection = PostgreSQLConnection(
+        Platform.environment['DB_HOST'] ?? 'localhost',
+        int.parse(Platform.environment['DB_PORT'] ?? '5432'),
+        Platform.environment['DB_NAME'] ?? 'user_management',
+        username: Platform.environment['DB_USER'] ?? 'luffy7132',
+        password: Platform.environment['DB_PASSWORD'] ?? 'password7132',
+      );
+      await _connection!.open();
     }
-    return _connection;
+
+    return _connection!;
+  }
+
+  static Future<void> closeConnection() async {
+    if (_connection != null && !_connection!.isClosed) {
+      await _connection!.close();
+    }
   }
 }
