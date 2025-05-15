935e18e0d364f56644711276a3f31bedd19a164b
harshit-7132 <harshitvyas880@gmail.com>
2025-05-15 12:12:50 +0530

    Deploy to Railway

--- lib/db.dart
+++ lib/db.dart
@@ -5,9 +5,9 @@ class Database {
   static final _connection = PostgreSQLConnection(
     Platform.environment['DB_HOST'] ?? 'localhost',
     int.parse(Platform.environment['DB_PORT'] ?? '5432'),
-    Platform.environment['DB_NAME'] ?? 'your_db_name',
-    username: Platform.environment['DB_USER'] ?? 'your_username',
-    password: Platform.environment['DB_PASSWORD'] ?? 'your_password',
+    Platform.environment['DB_NAME'] ?? 'user_management',  
+    username: Platform.environment['DB_USER'] ?? 'luffy7132',
+    password: Platform.environment['DB_PASSWORD'] ?? 'password7132',
   );
 
   static Future<PostgreSQLConnection> getConnection() async {
